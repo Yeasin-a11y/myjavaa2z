3694f1014e3dfd74c530cf8978776e0418ae4d6b
yeasin arafat <34199443+raghibboss@users.noreply.github.com>
2017-12-12 02:38:32 -0500

    Add files via upload

---
+++ demo2.java
@@ -0,0 +1,13 @@
+
+public class demo2 {
+
+	public static void main(String []args){
+	      String s1 = "tutorialspoint";
+	      String s2 = "tutorialspoint";
+	      String s3 = new String ("Tutorials Point");
+	      System.out.println(s1.equals(s2));
+	      System.out.println(s2.equals(s3));
+
+	}
+
+}
---
+++ demo3.java
@@ -0,0 +1,13 @@
+
+public class demo3 {
+
+	  public static void main(String []args){
+	      String s1 = "tutorialspoint";
+	      String s2 = "tutorialspoint";
+	      String s3 = new String ("Tutorials Point");
+	      System.out.println(s1 == s2);
+	      System.out.println(s2 == s3);
+	   }
+	}
+
+
